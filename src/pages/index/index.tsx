f76ab67d2b87063270358fbc6bdc847c29e61bdc
sunshine <jiuling.guo@bizatmobile.com>
2021-09-13 10:57:15 +0800

    .

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {View} from 'remax/one';
+// import { createCameraContext } from 'remax/wechat';
+import { usePageEvent, useAppEvent } from 'remax/macro';
 import './index.scss';
 import {useEffect} from "react";
 import {loadVideo, mobile, videoSize} from "@/utils";
@@ -9,7 +11,7 @@ import {initSketchGui, setupSketch} from "@/sketch";
 
 export default () => {
 
-    loadVideo().then(video => {
+    // loadVideo().then(video => {
         // const gui = new dat.GUI({width: 250});
         // const detectionGui = gui.addFolder('detection');
         // const sketchGui = gui.addFolder('sketch');
@@ -21,6 +23,17 @@ export default () => {
         //     initSketchGui(sketchGui);
         // });
         // poseDetection.initGui(detectionGui);
+    // })
+    usePageEvent('onReady', () => {
+        console.log('onReady', 3);
+        // const camera = createCameraContext();
+        // console.log(camera);
+    })
+    usePageEvent('onShow', () => {
+        console.log('onShow', 2);
+    })
+    usePageEvent('onLoad', () => {
+        console.log('onLoad', 1);
     })
 
     return (
